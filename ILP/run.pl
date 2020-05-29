59500a5e1399c174f9489c05ed5f4350fad52d52
dany varghese <gagiprha@gmail.com>
2020-05-30 00:24:36 +0100

    Update run.pl

--- ILP/run.pl
+++ ILP/run.pl
@@ -1,7 +1,7 @@
 :- use_module('source/gilps').
 % reads problem definition and saves learned theory in a file (this is optional)
 char:-
-  read_problem('datasets/ilptest').
+  read_problem('datasets/ilp').
   
 :- char.
 :- build_theory.
