93d3a7858ea1df65212df4902f93d258e2936375
dany varghese <gagiprha@gmail.com>
2020-05-30 00:24:10 +0100

    Delete demo_train_model.pl

--- demo_train_model.pl
+++
@@ -1,7 +0,0 @@
-:- use_module('source/gilps').
-% reads problem definition and saves learned theory in a file (this is optional)
-char:-
-  read_problem('datasets/ilptest').
-  
-:- char.
-:- build_theory.
