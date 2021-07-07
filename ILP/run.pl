5a78c2382c95811695c24d01cb8fb2243fa6aadd
Dany Varghese <gagiprha@gmail.com>
2021-07-07 11:49:59 +0100

    Delete ILP directory

--- ILP/run.pl
+++
@@ -1,7 +0,0 @@
-:- use_module('source/gilps').
-% reads problem definition and saves learned theory in a file (this is optional)
-char:-
-  read_problem('datasets/OSHD').
-  
-:- char.
-:- build_theory.
